82715a45ef8cc243ae67b0cdd1854bf436778f0d
RyongJoon <k95yong@naver.com>
2025-06-09 00:27:29 +0900

    250608 logger 수정

--- config/settings.py
+++ config/settings.py
@@ -1,20 +1,26 @@
 import logging
 import logging.handlers
 import os
+import sys
 
 from app.utils.path_util import get_root_dir
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 VIDEO_SOURCE_DIR = os.path.join(BASE_DIR, "temp_videos")
+
 LOG_DIR = os.path.join(get_root_dir(), 'logs')
+STDOUT_LOG_PATH = os.path.join(LOG_DIR, "stdout.log")
+STDERR_LOG_PATH = os.path.join(LOG_DIR, "stderr.log")
 LOG_FILE_PATH = os.path.join(LOG_DIR, 'app.log')
+
 UPLOAD_DIR = os.path.join(get_root_dir(), 'temp', 'uploads')
 MERGED_PDF_DIR = os.path.join(get_root_dir(), 'temp', 'merged_pdfs')
 SECRET_KEY = 'app-secret-key'
 os.makedirs(LOG_DIR, exist_ok=True)
 
+
 def setup_logger():
-    logger = logging.getLogger("my_app")
+    logger = logging.getLogger("flask_app")
     logger.setLevel(logging.INFO)
 
     if logger.handlers:
@@ -36,3 +42,6 @@ def setup_logger():
 
 
 logger = setup_logger()
+
+sys.stdout = open(STDOUT_LOG_PATH, 'a')
+sys.stderr = open(STDERR_LOG_PATH, 'a')
